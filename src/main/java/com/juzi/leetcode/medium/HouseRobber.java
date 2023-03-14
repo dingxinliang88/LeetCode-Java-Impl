5456c793ca0cfbf9c996c852a6ac06bfd57eb367
CodeJuzi <d1741530592@163.com>
2023-03-14 08:58:04 +0800

    [add] implement LeetCode 198 && LeetCode 213

---
+++ src/main/java/com/juzi/leetcode/medium/HouseRobber.java
@@ -0,0 +1,29 @@
+package com.juzi.leetcode.medium;
+
+/**
+ * <p>LeetCode 198</p>
+ * You are a professional robber planning to rob houses along a street.
+ * Each house has a certain amount of money stashed,
+ * the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ *
+ * Given an integer array nums representing the amount of money of each house,
+ * return the maximum amount of money you can rob tonight without alerting the police.
+ *
+ * 本题解法：动态规划
+ * @author codejuzi
+ */
+public class HouseRobber {
+    public int rob(int[] nums) {
+        if(nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[nums.length - 1];
+    }
+}
